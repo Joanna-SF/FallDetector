e30b2f125522bb58e04325ae2662753350ddfbdb
Joana Fernandes <joanasfernandes02@gmail.com>
2023-11-27 11:37:13 +0000

    First commit

---
+++ app/src/main/java/org/falldetectives/falldetector/MainActivity.java
@@ -0,0 +1,49 @@
+package org.falldetectives.falldetector;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonSendMessage = findViewById(R.id.buttonSendMessage);
+        buttonSendMessage.setOnClickListener(this::sendMessage);
+
+        // Check for SMS permission
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.SEND_SMS}, 1);
+        }
+    }
+
+    public void sendMessage(View v) {
+        EditText editTextPhoneNumber = findViewById(R.id.editTextPhone);
+        String phoneNumber = editTextPhoneNumber.getText().toString();
+        String message = "Your loved one needs help!";
+
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phoneNumber, null, message, null, null);
+            Toast.makeText(getApplicationContext(), "Message sent successfully", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "Message failed to send", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+}
