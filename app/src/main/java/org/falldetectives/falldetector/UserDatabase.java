d3dd70f98257339aba02742a6160cfc94fa26b2b
Constanca_Silva <constanca.i.silva@gmail.com>
2023-12-09 17:16:52 +0000

    Define user database and user model

--- app/src/main/java/org/falldetectives/falldetector/UserDatabase.java
+++ app/src/main/java/org/falldetectives/falldetector/UserDatabase.java
@@ -1,4 +1,57 @@
 package org.falldetectives.falldetector;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
-public class UserDatabase {
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDatabase extends SQLiteOpenHelper {
+
+    public static final String USER_TABLE = "User_Table";
+    public static final String COLUMN_USER_NAME = "USER_NAME";
+    public static final String COLUMN_USER_AGE = "USER_AGE";
+    public static final String COLUMN_USER_BLOOD_TYPE = "USER_BLOOD_TYPE";
+    public static final String COLUMN_USER_EMERGENCY_CONTACT = "USER_EMERGENCY_CONTACT";
+    public static final String COLUMN_ID = "ID";
+
+    public UserDatabase(@Nullable Context context) {
+        super(context, "user.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTableStatement = "CREATE TABLE " + USER_TABLE + " (" +
+                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_USER_NAME + " TEXT, " +
+                COLUMN_USER_AGE + " TEXT, " +
+                COLUMN_USER_BLOOD_TYPE + " TEXT, " +
+                COLUMN_USER_EMERGENCY_CONTACT + " TEXT)";
+        db.execSQL(createTableStatement);
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public boolean addOne(UserModel userModel) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(COLUMN_USER_NAME, userModel.getName());
+        cv.put(COLUMN_USER_AGE, userModel.getAge());
+        cv.put(COLUMN_USER_BLOOD_TYPE, userModel.getBlood_type());
+        cv.put(COLUMN_USER_EMERGENCY_CONTACT, userModel.getEmergency_contact());
+
+        long insert = db.insert(USER_TABLE, null, cv);
+        if (insert == -1) {
+            return false;
+        } else {
+            return true;
+        }
+
+    }
 }
