89eb9973d6ff04198348837cfdf8a69af9273169
Constanca_Silva <constanca.i.silva@gmail.com>
2023-12-09 18:36:02 +0000

    Connect selected user info to main activity display

--- app/src/main/java/org/falldetectives/falldetector/MainActivity.java
+++ app/src/main/java/org/falldetectives/falldetector/MainActivity.java
@@ -12,18 +12,20 @@ import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
+    TextView personEmergencyContact;
     private static final int COUNTDOWN_REQUEST_CODE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        personEmergencyContact = findViewById(R.id.editTextPhone);
         Button buttonSendMessage = findViewById(R.id.buttonSendMessage);
         buttonSendMessage.setOnClickListener(this::sendMessage);
 
@@ -32,6 +34,15 @@ public class MainActivity extends AppCompatActivity {
                 != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(MainActivity.this,
                     new String[]{Manifest.permission.SEND_SMS}, 1);
+        }
+
+        Intent intent = getIntent();
+        if (intent != null && intent.hasExtra("SELECTED_USER")) {
+            UserModel selectedUser = (UserModel) intent.getSerializableExtra("SELECTED_USER");
+
+            personEmergencyContact.setText(String.valueOf(selectedUser.getEmergency_contact()));
+
+
         }
     }
 
