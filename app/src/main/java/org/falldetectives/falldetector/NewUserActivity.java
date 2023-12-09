cf0786615db9dd9963df5ca7b15ce435f9adc222
Constanca_Silva <constanca.i.silva@gmail.com>
2023-12-09 18:09:10 +0000

    Add User to database method and display users in database method

--- app/src/main/java/org/falldetectives/falldetector/NewUserActivity.java
+++ app/src/main/java/org/falldetectives/falldetector/NewUserActivity.java
@@ -1,14 +1,48 @@
 package org.falldetectives.falldetector;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NewUserActivity extends AppCompatActivity {
-
+    private EditText editTextName, editTextAge, editTextBloodType, editTextEmergencyContact;
+    private Button button3;
+    private UserDatabase userDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user);
+        editTextName = findViewById(R.id.editTextName);
+        editTextAge = findViewById(R.id.editTextAge);
+        editTextBloodType = findViewById(R.id.editTextBloodType);
+        editTextEmergencyContact = findViewById(R.id.editTextEmergencyContact);
+        button3 = findViewById(R.id.button3);
+
+        userDatabase = new UserDatabase(NewUserActivity.this);
+
+        // Button Register- add to database
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+
+
+                UserModel userModel = new UserModel(-1,editTextName.getText().toString(),editTextAge.getText().toString(),editTextBloodType.getText().toString(),Integer.parseInt(editTextEmergencyContact.getText().toString()));
+                Toast.makeText(NewUserActivity.this, userModel.toString(), Toast.LENGTH_SHORT).show();
+
+                UserDatabase userdataBase = new UserDatabase(NewUserActivity.this);
+                boolean success = userdataBase.addOne(userModel);
+
+                Toast.makeText(NewUserActivity.this, "Success ="+success, Toast.LENGTH_SHORT).show();
+
+            }
+
+        });
+
 
 
     }
--- app/src/main/java/org/falldetectives/falldetector/RegisteredUserActivity.java
+++ app/src/main/java/org/falldetectives/falldetector/RegisteredUserActivity.java
@@ -1,15 +1,27 @@
 package org.falldetectives.falldetector;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.List;
+
 public class RegisteredUserActivity extends AppCompatActivity {
+    ListView user_list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registered_user);
 
+        user_list = findViewById(R.id.user_list);
 
+        // Retrieve and display the user list
+        UserDatabase userDatabase = new UserDatabase(RegisteredUserActivity.this);
+        List<UserModel> everyone = userDatabase.getEveryone();
+        ArrayAdapter<UserModel> userArrayAdapter = new ArrayAdapter<>(RegisteredUserActivity.this, android.R.layout.simple_list_item_1, everyone);
+        user_list.setAdapter(userArrayAdapter);
     }
 }
